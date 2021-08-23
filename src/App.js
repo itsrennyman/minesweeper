01021438303cae940530c09b3f9cdc8880ff8734
Renato Pozzi <r.pozzi@bluecube.it>
2021-08-23 22:31:20 +0200

    added tailwind & setup some css

--- src/App.js
+++ src/App.js
@@ -3,26 +3,36 @@ import { Tile } from "./components/Tile";
 
 const styles = {
   display: "grid",
-  gridTemplateColumns: "repeat(20, 40px)",
+  gridTemplateColumns: "repeat(10, 40px)",
   columnGap: "5px",
   rowGap: "5px",
 };
 
 export function App() {
-  const { matrix, showTile } = useGame(10, 20, 10);
+  const { matrix, showTile } = useGame(10, 10, 10);
 
   return (
-    <div>
-      <div>Minesweeper</div>
+    <section class="w-full px-6 pb-12 antialiased bg-white">
+      <div class="mx-auto max-w-7xl">
+        <div class="container max-w-lg px-4 py-16 mx-auto text-left md:max-w-none md:text-center">
+          <h1 class="text-5xl font-extrabold leading-10 tracking-tight text-left text-gray-900 md:text-center sm:leading-none md:text-6xl lg:text-7xl">
+            <span class="relative mt-2 text-transparent bg-clip-text bg-gradient-to-br from-indigo-600 to-indigo-500 md:inline-block">
+              Minesweeper
+            </span>
+          </h1>
+        </div>
 
-      <div style={styles}>
-        {matrix.map((row, y) =>
-          row.map((tile, x) => (
-            <Tile onClick={() => showTile(tile)} key={x + y} {...tile} />
-          ))
-        )}
+        <div class="container max-w-lg px-4 flex justify-center md:max-w-none">
+          <div style={styles}>
+            {matrix.map((row, y) =>
+              row.map((tile, x) => (
+                <Tile onClick={() => showTile(tile)} key={x + y} {...tile} />
+              ))
+            )}
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 }
 
--- src/components/Tile.js
+++ src/components/Tile.js
@@ -4,7 +4,9 @@ export const Tile = (props) => {
   if (!hidden) {
     return (
       <div
-        style={{ height: 40, width: 40, background: mine ? "#ff0000" : "#eee" }}
+        className={`h-10 w-10 flex items-center justify-center rounded shadow-sm text-sm font-bold ${
+          mine ? "bg-red-500" : "bg-gray-100"
+        }`}
       >
         <div>{value}</div>
       </div>
@@ -14,7 +16,7 @@ export const Tile = (props) => {
   return (
     <button
       onClick={props.onClick}
-      style={{ height: 40, width: 40, background: "#eee" }}
+      className="h-10 w-10 inline-flex items-center border border-gray-300 rounded shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
     >
       {value}
     </button>
